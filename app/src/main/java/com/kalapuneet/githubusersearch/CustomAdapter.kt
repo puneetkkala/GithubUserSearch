06cc0f86b649b17ada07c9e4e36fe412cfaf634a
puneet kala <puneet@docsapp.in>
2017-09-28 23:11:13 +0530

    made the changes to reduce the number of network calls

--- app/src/main/java/com/kalapuneet/githubusersearch/CustomAdapter.kt
+++ app/src/main/java/com/kalapuneet/githubusersearch/CustomAdapter.kt
@@ -6,11 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class CustomAdapter(val userList: Array<RestApiCall.User>?) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+class CustomAdapter : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    var userList: Array<RestApiCall.User>? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomAdapter.ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.row_user, parent, false)
-        return ViewHolder(v)
+        var itemView: View  = LayoutInflater.from(parent.context).inflate(R.layout.row_user, parent, false)
+        return ViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: CustomAdapter.ViewHolder, position: Int) {
@@ -19,13 +21,16 @@ class CustomAdapter(val userList: Array<RestApiCall.User>?) : RecyclerView.Adapt
 
     override fun getItemCount(): Int {
         if (userList != null) {
-            return userList.size
+            return userList!!.size
         }
         return 0
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun setItems(users: Array<RestApiCall.User>?) {
+        userList = users
+    }
 
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bindItems(user: RestApiCall.User) {
             val textViewName = itemView.findViewById<AppCompatTextView>(R.id.username)
             textViewName.text = user.login
--- app/src/main/java/com/kalapuneet/githubusersearch/MainActivity.kt
+++ app/src/main/java/com/kalapuneet/githubusersearch/MainActivity.kt
@@ -1,38 +1,36 @@
 package com.kalapuneet.githubusersearch
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.AppCompatEditText
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.text.Editable
-import android.text.TextWatcher
-import kotlinx.android.synthetic.main.activity_main.*;
+import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
-    fun AppCompatEditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
-        this.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun afterTextChanged(editable: Editable?) {
-                afterTextChanged.invoke(editable.toString())
-            }
-        })
-    }
+    lateinit var adapter: CustomAdapter
+    val handler = Handler()
+    lateinit var prev: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        search_result_rv.layoutManager = LinearLayoutManager(this)
-        search_name_et.afterTextChanged {
-            val users: Call<RestApiCall.SearchResponse> = RestApiCall.GithubService.createGithubService().getUsers(it)
+        adapter = CustomAdapter()
+        adapter.setItems(arrayOf())
+        search_result_rv.adapter = adapter
+        search_result_rv.layoutManager = LinearLayoutManager(this) as RecyclerView.LayoutManager?
+        prev = ""
+        makeNetworkCall()
+    }
+
+    fun makeNetworkCall() {
+        val query = search_name_et.text.toString()
+        if (!query.isEmpty() && !query.contentEquals(prev)) {
+            val users: Call<RestApiCall.SearchResponse> = RestApiCall.GithubService.createGithubService().getUsers(query)
             users.enqueue(object: Callback<RestApiCall.SearchResponse> {
                 override fun onFailure(call: Call<RestApiCall.SearchResponse>?, t: Throwable?) {
                     t?.printStackTrace()
@@ -40,14 +38,14 @@ class MainActivity : AppCompatActivity() {
 
                 override fun onResponse(call: Call<RestApiCall.SearchResponse>?, response: Response<RestApiCall.SearchResponse>?) {
                     val item = response?.body()
-                    val userList = item?.users
-                    runOnUiThread {
-                        val adapter = CustomAdapter(userList)
-                        search_result_rv.adapter = adapter
-                    }
+                    adapter.setItems(item?.users)
+                    adapter.notifyDataSetChanged()
                 }
-
             })
         }
+        prev = query
+        handler.postDelayed({
+            makeNetworkCall()
+        },1000)
     }
 }
